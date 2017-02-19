28a3cb40bd162404e31146b556d3e1ffe3c8513f
Stephen Wright <sjw948@gmail.com>
2017-02-19 21:59:04 +0000

    added overlay & links to work & code labs

--- js/main.js
+++ js/main.js
@@ -20,3 +20,34 @@
       });
     } // End if
   });
+
+// add overlay
+
+const addOverlay = () => {
+    const overlayDiv = document.querySelectorAll(`.image-container`)
+      for(let i = 0; i < overlayDiv.length; i+= 1) {
+        overlayDiv[i].addEventListener(`mouseenter`, () => {
+            let overlay = overlayDiv[i].querySelector(`.overlay`);
+            let overlayLinks = overlayDiv[i].querySelector(`.overlay-links`)
+            overlay.style.height = `calc(100% - 4px)`;
+            overlayLinks.style.visibility = `visible`;
+            overlayLinks.style.opacity = `1`;
+        })
+        overlayDiv[i].addEventListener(`mouseleave`, () => {
+            let overlay = overlayDiv[i].querySelector(`.overlay`);
+            let overlayLinks = overlayDiv[i].querySelector(`.overlay-links`)
+            overlay.style.height = `0%`;
+            overlayLinks.style.visibility = `invisible`;
+            overlayLinks.style.opacity = `0`;
+      })
+    }
+  }
+
+window.addEventListener('resize', () => {
+  const bodyWidth = window.innerWidth;
+  if(bodyWidth > `768`) {
+    addOverlay();
+  }
+})
+
+addOverlay();
