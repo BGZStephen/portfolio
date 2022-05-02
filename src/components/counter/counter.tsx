701a578921f3a650bc076898b40625b9e2eb8573
Stephen <sjw948@gmail.com>
2022-05-02 19:05:44 +0100

    simplify counter implementation

--- src/components/counter/counter.tsx
+++ src/components/counter/counter.tsx
@@ -1,35 +1,81 @@
-import React, { useState } from "react";
-import CounterDisplay, { CounterDisplayType } from "../counter-display.tsx/counter-display";
+import { differenceInSeconds, differenceInMinutes, differenceInHours, differenceInCalendarDays, differenceInMonths, differenceInYears } from "date-fns";
+import React, { useEffect, useState } from "react";
 
-function Counter() {
-  const localDate = new Date("2017-02-24")
-  const nowUtc = Date.UTC(localDate.getUTCFullYear(), localDate.getUTCMonth(), localDate.getUTCDate(),
-    localDate.getUTCHours(), localDate.getUTCMinutes(), localDate.getUTCSeconds());
-  const date = new Date(nowUtc)
+interface CounterDials { 
+  seconds: number;
+  minutes: number;
+  hours: number;
+  days: number;
+  months: number;
+  years: number;
+}
 
-  console.log(date)
+const Counter: React.FC<{}> = () => {
+  const [startDate] = useState<Date>(getStartDate())
+  const [dials, setDials] = useState<CounterDials>(calculateDials())
 
-  const [startDate] = useState(date)
+  function getStartDate(): Date {
+    const localDate = new Date("2017-02-24")
+    const nowUtc = Date.UTC(localDate.getUTCFullYear(), localDate.getUTCMonth(), localDate.getUTCDate(),
+      localDate.getUTCHours(), localDate.getUTCMinutes(), localDate.getUTCSeconds());
+    return new Date(nowUtc)
+  }
+
+  function getCurrentDateUtc(): Date {
+    const localDate = new Date()
+    const nowUtc = Date.UTC(localDate.getFullYear(), localDate.getMonth(), localDate.getDate(),
+      localDate.getHours(), localDate.getMinutes(), localDate.getSeconds());
+    return new Date(nowUtc)
+  }
+
+  function calculateDials(): CounterDials {
+    const currentDateUtc = getCurrentDateUtc()
+
+    const res: CounterDials = {
+      seconds: 0,
+      minutes: 0,
+      hours: 0,
+      days: 0,
+      months: 0,
+      years: 0
+    }
+
+    res.seconds = differenceInSeconds(currentDateUtc, startDate) % 60;
+    res.minutes = differenceInMinutes(currentDateUtc, startDate) % 60;
+    res.hours = differenceInHours(currentDateUtc, startDate) % 24;
+    res.days = differenceInCalendarDays(currentDateUtc, startDate) % 12;
+    res.months = differenceInMonths(currentDateUtc, startDate) % 12;
+    res.years = differenceInYears(currentDateUtc, startDate);
+
+    return res;
+  }
+
+  useEffect(() => {
+    const interval = setInterval(() => setDials(calculateDials()), 1000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
 
   return (
     <div className="counter">
       <div className="years">
-        <CounterDisplay startDate={startDate} type={CounterDisplayType.YEARS} />
+        {dials.years}
       </div>
       <div className="months">
-        <CounterDisplay startDate={startDate} type={CounterDisplayType.MONTHS} />
+        {dials.months}
       </div>
       <div className="days">
-        <CounterDisplay startDate={startDate} type={CounterDisplayType.DAYS} />
+        {dials.days}
       </div>
       <div className="hours">
-        <CounterDisplay startDate={startDate} type={CounterDisplayType.HOURS} />
+        {dials.hours}
       </div>
       <div className="minutes">
-        <CounterDisplay startDate={startDate} type={CounterDisplayType.MINUTES} />
+        {dials.minutes}
       </div>
       <div className="seconds">
-        <CounterDisplay startDate={startDate} type={CounterDisplayType.SECONDS} />
+        {dials.seconds}
       </div>
     </div>
   )
